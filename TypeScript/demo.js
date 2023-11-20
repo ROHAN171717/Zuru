276e432839ca7b34669ee8b172addff374816c51
ROHAN171717 <rohanlakhani@gmail.com>
2023-11-20 15:44:44 +0530

    unnecessary file deleted

--- TypeScript/demo.js
+++
@@ -1,9 +0,0 @@
-// using as syntax
-var x = {};
-// x.firstname = "random"
-x = "rg";
-console.log(typeof x);
-// asserting it as Person using as syntax
-var firstname = x.firstname;
-// asserting it as Person using the angle brackets
-var newfirstname = x.firstname;
